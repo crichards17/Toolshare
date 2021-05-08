9e1c97404c10ac8ef701fc5b11f4d1bfc2c63aba
Aaron <aaronthesnowboarder@live.com>
2021-05-08 12:23:22 -0700

    worked on relationships

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const User = require('./User');
 const Tool = require('./Tool');
 const ToolType= require('./ToolType');
 const ToolTags= require('./ToolTags')
+const ToolModel= require('./ToolModel')
+const ToolMake = require('./ToolMake')
 
 User.hasMany(Tool,{
     foreignKey:'user_id',
@@ -11,15 +13,28 @@ User.hasMany(Tool,{
 Tool.belongsTo(User,{
     foreignKey:'user_id'
 });
-
-ToolType.belongsToMany(Tool,{
-    through:{
-        model:ToolTags
-    }
+ToolType.hasMany(Tool,{
+    foreignKey:' tool_type_id'
 })
+
+ToolModel.hasMany(Tool,{
+    foreignKey:'tool_model_id'
+})
+
+ToolMake.hasMany(Tool,{
+    foreignKey:'tool_make_id'
+})
+
+Tool.belongsTo(ToolModel,{
+    foreignKey:'tool_model_id'
+});
 
 Tool.belongsTo(ToolType,{
-    through:{
-        model:ToolTags
-    }
+    foreignKey:' tool_type_id'
 })
+
+Tool.belongsTo(ToolMake,{
+    foreignKey:'tool_make_id'
+})
+
+
