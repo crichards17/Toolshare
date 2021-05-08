f30127623e0ed23483136d5527f44f728d4dad9f
Aaron <aaronthesnowboarder@live.com>
2021-05-08 12:47:28 -0700

    updates

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const User = require('./User');
 const Tool = require('./Tool');
-const ToolType= require('./ToolType');
-const ToolTags= require('./ToolTags')
-const ToolModel= require('./ToolModel')
-const ToolMake = require('./ToolMake')
+// const ToolType= require('./ToolType');
+// const ToolTags= require('./ToolTags')
+// const ToolModel= require('./ToolModel')
+// const ToolMake = require('./ToolMake')
 
 User.hasMany(Tool,{
     foreignKey:'user_id',
@@ -13,28 +13,28 @@ User.hasMany(Tool,{
 Tool.belongsTo(User,{
     foreignKey:'user_id'
 });
-ToolType.hasMany(Tool,{
-    foreignKey:' tool_type_id'
-})
+// ToolType.hasMany(Tool,{
+//     foreignKey:' tool_type_id'
+// })
 
-ToolModel.hasMany(Tool,{
-    foreignKey:'tool_model_id'
-})
+// ToolModel.hasMany(Tool,{
+//     foreignKey:'tool_model_id'
+// })
 
-ToolMake.hasMany(Tool,{
-    foreignKey:'tool_make_id'
-})
+// ToolMake.hasMany(Tool,{
+//     foreignKey:'tool_make_id'
+// })
 
-Tool.belongsTo(ToolModel,{
-    foreignKey:'tool_model_id'
-});
+// Tool.belongsTo(ToolModel,{
+//     foreignKey:'tool_model_id'
+// });
 
-Tool.belongsTo(ToolType,{
-    foreignKey:' tool_type_id'
-})
+// Tool.belongsTo(ToolType,{
+//     foreignKey:' tool_type_id'
+// })
 
-Tool.belongsTo(ToolMake,{
-    foreignKey:'tool_make_id'
-})
+// Tool.belongsTo(ToolMake,{
+//     foreignKey:'tool_make_id'
+// })
 
 
