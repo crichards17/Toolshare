658caf73cb2a61ed02bab19c7937c893cd23a87e
ru14 <74753345+ru14@users.noreply.github.com>
2021-05-10 13:03:04 -0700

    home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,3 +27,57 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/Tool/:id', async (req, res) => {
+    try {
+      const ToolData = await Tool.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const Tool = ToolData.get({ plain: true });
+  
+      res.render('Tool', {
+        ...Tool,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // Use withAuth middleware to prevent access to route
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Tool }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
+  
