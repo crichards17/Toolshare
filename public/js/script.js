48cc39ed028ebac1af1705cbefcad04e53b21062
Nathan Nosar <nathan.nosar@gmail.com>
2021-05-10 21:19:32 -0700

    selected tool page

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+  io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
 $(document).ready(function() {
  
     $("#owl-demo").owlCarousel({
@@ -18,4 +42,3 @@ $(document).ready(function() {
    
   });
 
-  
