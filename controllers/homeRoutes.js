ab9e448211057cf4f1d570f21f5471d0c116f0a6
ru14 <74753345+ru14@users.noreply.github.com>
2021-05-12 17:45:07 -0700

    some modification

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,6 +48,24 @@ router.get('/Tool/:id', async (req, res) => {
       res.status(500).json(err);
     }
   });
+  // Find the logged in user based on the session ID
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Tool }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   
 
   module.exports = router;
