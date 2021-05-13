30b990fd1d7d5943896848cb18dfdc2420298a8e
Aaron <aaronthesnowboarder@live.com>
2021-05-12 20:12:26 -0700

    merge

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
   
     if (email && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
