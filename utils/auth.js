14172bbcb64dd968589a1fba4653b5b2ec3e9ab2
ru14 <74753345+ru14@users.noreply.github.com>
2021-05-12 18:14:12 -0700

    error fixed

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,4 @@
-onst withAuth = (req, res, next) => {
+const withAuth = (req, res, next) => {
     // If the user is not logged in, redirect the request to the login route
     if (!req.session.logged_in) {
       res.redirect('/login');
