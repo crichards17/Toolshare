c3d90ad98b2008d7aa7dfaee11adad24abaa3122
Aaron <aaronthesnowboarder@live.com>
2021-05-15 11:25:09 -0700

    gets distance info

--- public/js/ToolScript.js
+++ public/js/ToolScript.js
@@ -1,13 +1,24 @@
-const filters=async()=>{
+const filters=async(event)=>{
+    
     event.preventDefault();
     const distance = document.querySelector('#distance')
+    console.log(distance.value)
+    const sendDistance= await fetch('/api/searchRoutes/Distance',{
+        method: 'POST',
+        body: distance.value,
+        headers: { 'Content-Type': 'application/json' },
+    })
 
-
+    if (sendDate.ok){
+        console.log('pass')
+    }
 }
 
 
 
-
+document
+    .querySelector('#apply_filters')
+    .addEventListener('click', filters)
 
 
 
