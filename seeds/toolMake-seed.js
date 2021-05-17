ce92a40d1c3ffcd0c2d2441b07c8d2105e0da242
Aaron <aaronthesnowboarder@live.com>
2021-05-17 11:32:47 -0700

    added more seed data

--- seeds/toolMake-seed.js
+++ seeds/toolMake-seed.js
@@ -1,16 +1,24 @@
 const ToolMake = require('../models/ToolMake')
 
 const toolMakeData= [
+    //MySQL Database, starts at 1 NOT 0
     {
         make_name:"Pimp Tools"
+        //1
         
     },
     {
         make_name:"Cool Tools"
+        //2
     },
     {
         make_name:"Super Tools"
-    }
+        //3
+    },
+    {
+        make_name:"Michigain State is Trash"
+        //4
+    },
 ]
 
 const seedToolMake=()=>ToolMake.bulkCreate(toolMakeData);
